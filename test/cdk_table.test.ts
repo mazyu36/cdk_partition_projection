d306df1b47b827bc8567d7d38c15a1fa9c94441e
maz <yiyth.fcb6@gmail.com>
2023-03-07 08:49:57 +0900

    Initial commit

---
+++ test/cdk_table.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTable from '../lib/cdk_table-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_table-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTable.CdkTableStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
